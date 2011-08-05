edc141ebefb979053e7ee0018c2bf12bb0b49b2c
zina01 <zina01@zina01-samsung.stichtingksc.lan>
2011-08-05 18:44:57 +0200

    putting cloning stuff to user zina.application@gmail.com

--- batchFiles/zinaApp_clone_code.bat
+++ batchFiles/zinaApp_clone_code.bat
@@ -1,2 +1,3 @@
+
 ::clone \zinaMercurial from google code
-::hg clone https://arneboon@code.google.com/p/zina-app-mercurial C:\Workspace\openFrameworks_0062\apps_arne\zinaMercurial\
+hg clone https://zina.application@code.google.com/p/zina-app-mercurial C:\Workspace\openFrameworks_0062\apps_arne\zinaMercurial\
