a1c5b24a94860ca55cdb185e84056a1bc69e74f1
zina01 <zina01@zina01-samsung.stichtingksc.lan>
2011-08-05 19:02:28 +0200

    - changing order in batch files - rename files

---
+++ batchFiles/step-1_zinaApp_copy.bat
@@ -0,0 +1,21 @@
+
+
+::COPY ZINA DATA FROM DRIVE AND CLONE FROM INTERNET TO DISK
+
+::copy \SOFTWARE
+mkdir c:\Software
+xcopy e:\ZINA_TO_NETBOOK\Software c:\Software /D /E /C /R /I /K /Y
+
+::copy \WORKSPACE
+mkdir c:\Workspace
+xcopy e:\ZINA_TO_NETBOOK\Workspace c:\Workspace /D /E /C /R /I /K /Y
+
+
+
+::copy \STARTUP LNK
+
+copy "C:\Workspace\START ZINA APP.lnk" "C:\Users\%username%\Desktop"
+
+
+
+copy "C:\Workspace\START ZINA APP.lnk" "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
--- batchFiles/zinaApp_clone_code.bat
+++
@@ -1,3 +0,0 @@
-
-::clone \zinaMercurial from google code
-hg clone https://zina.application@code.google.com/p/zina-app-mercurial C:\Workspace\openFrameworks_0062\apps_arne\zinaMercurial\
