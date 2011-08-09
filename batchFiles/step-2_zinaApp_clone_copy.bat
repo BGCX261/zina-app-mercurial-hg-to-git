82e467de1a5d6581c2a46155301d25257cf33c5b
zina01 <zina01@zina01-samsung.stichtingksc.lan>
2011-08-09 12:16:00 +0200

    hg clone: add folder: zina-app-mercurial - a folder deeper for oF to catch correct

--- batchFiles/step-2_zinaApp_clone_copy.bat
+++ batchFiles/step-2_zinaApp_clone_copy.bat
@@ -3,7 +3,7 @@
 ::first download code from google code, then add data from drive to project bin folder
 
 ::clone \zinaMercurial from google code
-hg clone https://zina.application@code.google.com/p/zina-app-mercurial C:\Workspace\openFrameworks_0062\apps_arne\zinaMercurial\
+hg clone https://zina.application@code.google.com/p/zina-app-mercurial C:\Workspace\openFrameworks_0062\apps_arne\zinaMercurial\zina-app-mercurial
 
 ::copy \data\stations
 mkdir C:\Workspace\openFrameworks_0062\apps_arne\zinaMercurial\bin\data\stations
