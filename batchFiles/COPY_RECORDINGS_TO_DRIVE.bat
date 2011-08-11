730c8870c801f67fb0dd19473f2d8131be3ad744
arneboon <arneboon@gmail.com>
2011-08-11 13:29:07 +0200

    - batch file copy recordings: forget about selecting drive

--- batchFiles/COPY_RECORDINGS_TO_DRIVE.bat
+++ batchFiles/COPY_RECORDINGS_TO_DRIVE.bat
@@ -2,11 +2,6 @@
 ::INFO
 ::copy recordings from disk to drive
 
-::TODO
-::prompt user to input drive letter
-
-set /p drive_letter= INPUT DRIVE LETTER TO COPY TO [d, e, f, g]:  
-
 set path_from=C:\Workspace\openFrameworks_0062\apps_arne\zinaMercurial\zina-app-mercurial\bin\data\recordings\ 
 set path_to=E:\ZINA_APP\voice_recordings\%username%\recordings\ 
 
